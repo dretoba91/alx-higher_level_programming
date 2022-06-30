18e915cf842c02dfeb1393e421e4a997ce8864ad
Dare Oloruntoba <64743870+dretoba91@users.noreply.github.com>
2022-06-30 21:09:44 +0100

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,7 @@
-q
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    definitions = dir(hidden_4)
+    for i in range(0, len(definitions)):
+        if definitions[i][0] != '_' and definitions[i][1] != '_':
+            print(definitions[i])
