7a1560a90ca260b4e7c2ec3e7e7fd4280cb4f4b9
Dare Oloruntoba <64743870+dretoba91@users.noreply.github.com>
2022-07-06 23:11:35 +0100

    Update 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,1 +1,9 @@
-q
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    replacement_list = []
+    for item in my_list:
+        if item == search:
+            replacement_list.append(replace)
+        else:
+            replacement_list.append(item)
+    return replacement_list
