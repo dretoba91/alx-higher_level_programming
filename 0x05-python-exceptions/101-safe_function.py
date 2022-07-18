7cb581b7314922fdabf22967bddb6a886ce1bc81
Dare Oloruntoba <64743870+dretoba91@users.noreply.github.com>
2022-07-18 23:19:17 +0100

    Update 101-safe_function.py

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,1 +1,12 @@
-q
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+
+    try:
+        result = fct(*args)
+        return result
+    except Exception as ex:
+        print("Exception: {}".format(ex), file=sys.stderr)
+        return None
