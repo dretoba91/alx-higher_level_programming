af6d1b0f9026c1049992f51cbf15bf1eeec452ef
Dare Oloruntoba <64743870+dretoba91@users.noreply.github.com>
2022-07-27 01:18:13 +0100

    Update 101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,1 +1,11 @@
-q
+#!/usr/bin/python3
+"""A module containing a restriced class.
+"""
+
+
+class LockedClass:
+    """LockedClass
+    This class is locked to only one attribute
+    which is the 'first_name'
+    """
+    __slots__ = ['first_name']
