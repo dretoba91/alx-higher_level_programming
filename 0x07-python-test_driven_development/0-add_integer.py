d5438ba325a4aff5cab2333443b929c65252d2f5
Dare Oloruntoba <64743870+dretoba91@users.noreply.github.com>
2022-07-29 00:59:45 +0100

    Update 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,1 +1,26 @@
-q
+#!/usr/bin/python3
+"""Python Integer addition module
+"""
+
+
+def add_integer(a, b=98):
+    """add_integer(a, b= 98)
+    The function takes two arguments a, b and returns the sum.
+    Args:
+        a (int, float): first argument
+        b (int, float): Second argument Defaults to 98.
+    Returns:
+        int: sum of both values
+    """
+
+    if type(a) is float:
+        a = int(a)
+    elif type(a) is not int:
+        raise TypeError('a must be an integer')
+
+    if type(b) is float:
+        b = int(b)
+    elif type(b) is not int:
+        raise TypeError('b must be an integer')
+
+    return a + 
