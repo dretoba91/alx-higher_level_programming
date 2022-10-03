73aa378f8b2c595b593a0cb3e0b813b83a0b9586
Dare Oloruntoba <64743870+dretoba91@users.noreply.github.com>
2022-10-03 22:18:13 +0100

    5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,1 +1,9 @@
-q
+#!/usr/bin/node
+
+const argsChecker = Number.parseInt(process.argv[2]);
+
+if (Number.isNaN(argsChecker)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + argsChecker);
+}
