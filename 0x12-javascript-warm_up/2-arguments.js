07a6446743cbc732c6aeab2d8adf86a3d9433c63
Dare Oloruntoba <64743870+dretoba91@users.noreply.github.com>
2022-10-03 22:15:05 +0100

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,1 +1,9 @@
-q
+#!/usr/bin/node
+
+const argsChecker = process.argv.length;
+
+if (argsChecker > 2) {
+  console.log('Argument' + ((argsChecker > 3) ? 's' : '') + ' found');
+} else {
+  console.log('No argument');
+}
