922260be3207093d73aa38fb37413597daf40b05
Dare Oloruntoba <64743870+dretoba91@users.noreply.github.com>
2022-10-04 22:46:33 +0100

    2-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,1 +1,12 @@
-q
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+
+module.exports = Rectangle;
